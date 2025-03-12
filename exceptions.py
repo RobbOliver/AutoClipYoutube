4b3c40b7250795e2c8c30395110994a324ec2033
RobbOliver <robsoncaetano@email.com>
2025-03-12 12:08:52 -0300

    refactor(exceptions) refac to load a new message

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,5 @@
 class ChannelIDNotFoundError(Exception):
-    pass
+    """Exceção lançada quando o Channel ID não puder ser encontrado a partir do handle."""
+
+    def __init__(self, message="Error to get ChannelID."):
+        super().__init__(message)
