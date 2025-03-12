989e0902d058342e92f8d2de82d60f41e4d72d75
RobbOliver <robsoncaetano@email.com>
2025-03-12 11:15:02 -0300

    chore(GetChanByHandle) complete the code

--- core/get_channelID_by_handle.py
+++ core/get_channelID_by_handle.py
@@ -1,5 +1,6 @@
 import requests
 from config import API_KEY_YT, YOUTUBER_CHANNEL
+from exceptions import ChannelIDNotFoundError
 
 
 def get_channelID_by_handle():
@@ -8,25 +9,27 @@ def get_channelID_by_handle():
 
     Returns:
         str: Channel ID se encontrado.
-        None: Se não encontrado ou ocorrer erro na requisição.
+
+    Raises:
+        ChannelIDNotFoundError: Se o canal não for encontrado ou ocorrer erro na requisição.
     """
 
     url = f"https://www.googleapis.com/youtube/v3/channels?part=id&forHandle={YOUTUBER_CHANNEL}&key={API_KEY_YT}"
 
     try:
-        response = requests.get(url)
-        response.raise_for_status() # Rise Error if not 200 status code
+        response = requests.get(url, timeout=10)  # To not get Locked
+        response.raise_for_status()  # Rise Error if not 200 status code
         data = response.json()
 
         items = data.get("items")
         if items and len(items) > 0:
             return items[0]["id"]
-        else:
-            return 
 
-        if data["items"]:
-            channelID = data["items"][0]["id"]
-            return channelID
-        else:
-            return None
-    except:
+        raise ChannelIDNotFoundError(
+            "❌ No channels found for this handle.")
+
+    except requests.RequestException as ex:
+        raise ChannelIDNotFoundError(f"❌ HTTP request error: {ex}")
+
+    except KeyError as ex:
+        raise ChannelIDNotFoundError(f"❌ Error - ID was not found: {ex}")
