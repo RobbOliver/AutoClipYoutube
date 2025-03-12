b286b4d994bec76b70d01da3001f25e23ce2f3d9
RobbOliver <robsoncaetano@email.com>
2025-03-12 09:03:10 -0300

    chore(config) add config.py with main variable

---
+++ main.py
@@ -0,0 +1,12 @@
+# No main eu vou:
+# 1. Importar o módulo de funções
+# 2. Chamar as seguintes funções:
+#    - Validar se a stream está online
+#    - Se sim, pegar o VideoID e ChannelID
+#    - Iniciar a gravação da live
+#    - Monitorar o chat da live
+#    - Verificar se o comando !clip foi digitado
+#    - Caso o comando !clip seja digitado, pegar o timestamp e criar o clip
+#    - Salvar o clip
+
+
