3af0b1ea43fcbb4424466be812fa38004f48dd1f
RobbOliver <robsoncaetano@email.com>
2025-03-12 19:01:30 -0300

    chore(exception) add videoIdNotFoundError

--- exceptions.py
+++ exceptions.py
@@ -3,3 +3,10 @@ class ChannelIDNotFoundError(Exception):
 
     def __init__(self, message="Error to get ChannelID."):
         super().__init__(message)
+
+
+class VideoIDNotFoundError(Exception):
+    """Exceção lançada quando o Video ID não puder ser encontrado a partir do Channel ID."""
+
+    def __init__(self, message="Error to get VideoID."):
+        super().__init__(message)
