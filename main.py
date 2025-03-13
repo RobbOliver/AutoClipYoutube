2d9a6cd2993d4d236db61f4c9000cfdec5043ab4
RobbOliver <robsoncaetano@email.com>
2025-03-13 14:12:27 -0300

    feat(main) load all data and start the program

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-# No main eu vou:
-# 1. Importar o módulo de funções
-# 2. Chamar as seguintes funções:
-#    - Validar se a stream está online
-#    - Se sim, pegar o VideoID e ChannelID
-#    - Iniciar a gravação da live
-#    - Monitorar o chat da live
-#    - Verificar se o comando !clip foi digitado
-#    - Caso o comando !clip seja digitado, pegar o timestamp e criar o clip
-#    - Salvar o clip
+import os
+from config import FFMPEG_PATH, YT_DLP_PATH
+from core.chat_listener import start_monitor_chat
+from core.get_channelID_by_handle import get_channelID_by_handle
+from core.get_videoID import get_videoID_by_channelID
+from core.get_liveChatID import get_liveChatID
+from core.live_recording import start_live_recording
+from exceptions import ChannelIDNotFoundError, FFmpegNotFoundError, VideoIDNotFoundError, YTDLNotFoundError
 
 
+def main():
+    try:
+        print("🔎 Buscando Channel ID...")
+        data_streamer = get_channelID_by_handle()
+        print(f"✅ Channel ID encontrado...")
+
+        print("🎥 Verificando se há live ativa...")
+        data_streamer = get_videoID_by_channelID(data_streamer)
+
+        print("🔎 Buscando Live Chat ID...")
+        data_streamer = get_liveChatID(data_streamer)
+        print(f"✅ Live ativa detectada!...")
+
+        print(f"✅ Live Chat ID encontrado")
+
+        if not os.path.exists(FFMPEG_PATH):
+            raise YTDLNotFoundError(
+                message="yt-dlp não encontrado. Verifique o caminho!")
+
+        if not os.path.exists(YT_DLP_PATH):
+            raise FFmpegNotFoundError(
+                message="ffmpeg não encontrado. Verifique o caminho!")
+
+        start_live_recording(data_streamer)
+        print("🚀 Iniciando o monitoramento do chat e gravação em backstage!")
+
+        print("✅ Monitorando o chat... Pressione Ctrl + C para parar.")
+        start_monitor_chat(data_streamer)
+
+    except ChannelIDNotFoundError as ce:
+        print(str(ce))
+    except VideoIDNotFoundError as ve:
+        print(str(ve))
+    except Exception as e:
+        print(f"❌ Erro inesperado: {e}")
+
+
+if __name__ == "__main__":
+    main()
