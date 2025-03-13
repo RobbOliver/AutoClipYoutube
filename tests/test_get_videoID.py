94ae85cbc70408635b6d28206ac99c94811b645e
RobbOliver <robsoncaetano@email.com>
2025-03-13 09:10:32 -0300

    feat(test_GetVideoID) add new test GetVideoID

--- tests/test_get_videoID.py
+++ tests/test_get_videoID.py
@@ -1,6 +1,10 @@
 from unittest.mock import patch
+
+import pytest
+import requests
 # ajuste conforme o nome correto
 from core.get_videoID import get_videoID_by_channelID
+from exceptions import VideoIDNotFoundError
 
 # Teste simulando resposta correta
 
@@ -20,3 +24,38 @@ def test_get_videoID_by_channelID_success(mock_get):
     result = get_videoID_by_channelID(data_streamer)
 
     assert result == expected_result
+
+
+# Teste simulando resposta sem ID (sem canais encontrados)
+
+
+@patch('core.get_videoID.requests.get')
+def test_get_videoID_by_channelID_not_found(mock_get):
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {
+        "items": []
+    }
+
+    with pytest.raises(VideoIDNotFoundError, match="❌ Nenhuma live ativa encontrada para este canal."):
+        get_videoID_by_channelID({"ChannelID": "EXEMPLO_CHANNEL_ID"})
+
+# Teste simulando erro de requisição (ex: conexão falhou)
+
+
+@patch('core.get_videoID.requests.get')
+def test_get_videoID_by_channelID_request_error(mock_get):
+    mock_get.side_effect = requests.RequestException("Erro de conexão")
+
+    with pytest.raises(VideoIDNotFoundError, match="❌ Erro na requisição HTTP: Erro de conexão"):
+        get_videoID_by_channelID({"ChannelID": "EXEMPLO_CHANNEL_ID"})
+
+# Teste simulando erro de processamento de dados
+
+
+@patch('core.get_videoID.requests.get')
+def test_get_videoID_by_channelID_data_error(mock_get):
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {}
+
+    with pytest.raises(VideoIDNotFoundError, match="❌ Nenhuma live ativa encontrada para este canal."):
+        get_videoID_by_channelID({"ChannelID": "EXEMPLO_CHANNEL_ID"})
